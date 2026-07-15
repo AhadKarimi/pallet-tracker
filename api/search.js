6b24b675c8ecbc189c40d1545bc7bf62bbe50144
Ahad <ahad.karimi8@gmail.com>
2026-07-15 18:20:44 -0400

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,12 @@
-
+const axios=require('axios');
+const cheerio=require('cheerio');
+module.exports=async(req,res)=>{
+try{
+const html=(await axios.get('https://www.walmart.ca/en/search?q='+req.query.barcode)).data;
+const $=cheerio.load(html);
+res.json({
+title:$('a[link-identifier] span').first().text().trim(),
+image:$('img').first().attr('src')||''
+});
+}catch(e){res.status(500).json({error:e.message});}
+};
